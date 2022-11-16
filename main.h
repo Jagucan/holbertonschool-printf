2940cbc49b0622141acb4e138ff073ad8d29fc73
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-16 11:38:16 -0500

    printf

--- main.h
+++ main.h
@@ -17,14 +17,5 @@ typedef struct print
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int _print_di(va_list elements);
-int _print_u(va_list elements);
-int _print_o(va_list elements);
-int _print_xX(va_list elements);
-int _print_c(va_list elements);
-int _print_s(va_list elements);
-int _print_p(va_list elements);
-int _print_percent(va_list elements);
-int _print_r(va_list elements);
 
 #endif
