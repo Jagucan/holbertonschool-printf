f2d67edbfc54b82d2356f25f51f513e15f1fad12
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-24 17:01:19 -0500

    printf

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 /**
  * _printf - Produces output according to a format.
  * @format: Is a character string.
- * return: Return the number of characters printed.
+ * return: Numbers of char printed.
 */
 int _printf(char *format, ...)
 {
