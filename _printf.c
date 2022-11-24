520028f6348571310c9e580704b33e603c29edbb
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-24 13:04:10 -0500

    printf

--- _printf.c
+++ _printf.c
@@ -3,18 +3,15 @@
 int _printf(char *format, ...)
 {
 	va_list ap;
-
 	size_t len = 0;
 	char buffer[2000];
 	int a = 0;
-	int (*cases_print)(va_list, char *, int );
+	int (*cases_print)(va_list, char *, int);
 
-	va_start (ap, format);
-	
+	va_start(ap, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0')
 		|| (format[0] == ' ' && format[1] == '\0'))
 		return (-1);
-
 	while (format[a])
 	{
 		if (format[a] != '%')
@@ -22,25 +19,21 @@ int _printf(char *format, ...)
 			buffer[len] = format[a];
 			len += 1;
 		}
-
 		else
 		{
 			cases_print = get_print_cases(&(format[a + 1]));
-
 			if (cases_print != NULL)
 			{
 				len = cases_print(ap, &buffer[len], len);
 				a++;
 			}
-
 			else
 			{
-			buffer[len] = format[a];
-						len += 1;
+				buffer[len] = format[a];
+				len += 1;
 			}
 		}
 		a++;
-
 	}
 	write(1, buffer, len);
 	va_end(ap);
