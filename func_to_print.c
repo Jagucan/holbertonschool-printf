18e7218d5bd18041de194c1e4145585df51e5b80
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-24 12:26:31 -0500

    printf

--- func_to_print.c
+++ func_to_print.c
@@ -6,10 +6,13 @@ int c_case(va_list ap, char *buff, int len)
 
 	c = malloc((sizeof(char) + 1));
 	if (c == NULL)
-	    exit(1);
+		exit(1);
+
 	c[0] = va_arg(ap, int);
+
 	if (c[0] == 0)
-		exit (1);
+		exit(1);
+
 	len = replace(buff, c, len);
 	free(c);
 	return (len);
@@ -17,13 +20,13 @@ int c_case(va_list ap, char *buff, int len)
 
 int s_case(va_list ap, char *buff, int len)
 {
-    	char *s;
+	char *s;
 	char *a;
 	int len_s;
 
 	a = va_arg(ap, char*);
 	if (a == NULL)
-		    exit(1);
+		exit(1);
 	len_s = _strlen(a);
 	s = malloc((sizeof(char) * len_s) + 1);
 	if (s == NULL)
@@ -37,9 +40,11 @@ int s_case(va_list ap, char *buff, int len)
 int percent_case(va_list ap, char *buff, int len)
 {
 	char *p;
+
 	p = malloc((sizeof(char) + 1));
+
 	if (p == NULL)
-		 return (1);
+		return (1);
 	p[0] = va_arg(ap, int);
 	p[0] = '%';
 	len = replace(buff, p, len);
