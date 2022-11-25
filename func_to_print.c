e3b731f23bdae85b170dd458a74bf439488fa7bc
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-25 17:44:32 -0500

    printf casi completo

--- func_to_print.c
+++ func_to_print.c
@@ -119,3 +119,41 @@ int di_case(va_list ap, char *buff, int len)
 	free(a);
 	return (len);
 }
+
+/**
+ * d_case - Print a decimal.
+ * @ap: Is an argument.
+ * @buff: Is the buffer.
+ * @len: Is the length of the string.
+ * Return: The length of the string.
+*/
+
+int u_case(va_list ap, char *buff, int len)
+{
+	char *s;
+	char *a;
+	int s_len;
+	unsigned int num;
+
+	num = va_arg(ap, int);
+	num = num * -1;
+
+	a = malloc((sizeof(char) * num) + 1);
+
+	if (a == NULL)
+		return (1);
+	
+	a = _itoa(num, a, len);
+	s_len = _strlen(a);
+
+	s = malloc((sizeof(char) * num) + 1);
+
+	if (s == NULL)
+		return (1);
+	
+	_strcpy(s, a);
+	len = replace(buff, s, len);
+	free(s);
+	free(a);
+	return (len);
+}
