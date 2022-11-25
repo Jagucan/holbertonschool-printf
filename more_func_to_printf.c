fe14367e08e08c1082b014fc55f352048492fcdf
Jacinto Gutierrez <jjgc.2499@gmail.com>
2022-11-25 18:01:15 -0500

    printf casi completo

---
+++ more_func_to_printf.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * d_case - Print a unsigned num.
+ * @ap: Is an argument.
+ * @buff: Is the buffer.
+ * @len: Is the length of the string.
+ * Return: The length of the string.
+*/
+
+int u_case(va_list ap, char *buff, int len)
+{
+	char *s;
+	char *a;
+	int s_len;
+	unsigned int num;
+
+	num = va_arg(ap, int);
+	num = num * -1;
+
+	a = malloc((sizeof(char) * num) + 1);
+
+	if (a == NULL)
+		return (1);
+	
+	a = _itoa(num, a, len);
+	s_len = _strlen(a);
+
+	s = malloc((sizeof(char) * num) + 1);
+
+	if (s == NULL)
+		return (1);
+	
+	_strcpy(s, a);
+	len = replace(buff, s, len);
+	free(s);
+	free(a);
+	return (len);
+}
